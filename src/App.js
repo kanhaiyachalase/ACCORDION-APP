79eec5c80689c820d24d204dddeeb0dbfcd1517e
kanhaiyachalase <79249131+kanhaiyachalase@users.noreply.github.com>
2021-05-27 09:12:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Accordion from './component/accordion/Accordion';
+// import Accordion from './component/accordion/Accordion';
+
+const App = () => {
+  return (
+    <>
+      <Accordion />
+    </>
+  )
+}
+export default App
